ede291e1917c5f89086ec12b70f631afec2e93c1
Roberto Rodríguez <rrb@denexus.io>
2025-06-13 19:56:30 +0200

    fix test

--- tests/acceptance/test_config.py
+++ tests/acceptance/test_config.py
@@ -1,8 +1,8 @@
 from pathlib import PosixPath
+
 from config import settings
 
 
 def test_config_class():
-    assert settings.data_path == PosixPath(
-        "/home/roberto/devel/python/fraud-dect/dataset"
-    )
+    assert isinstance(settings.data_path, PosixPath)
+    assert "dataset" in str(settings.data_path)
