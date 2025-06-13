54e4691bf975f814c4de87d38f7b7292ce197fd3
Roberto Rodríguez <rrb@denexus.io>
2025-06-13 19:53:06 +0200

    added tests to github CI

--- tests/acceptance/test_load_dataset.py
+++ tests/acceptance/test_load_dataset.py
@@ -1,7 +1,9 @@
+import pytest
 from utils.filesystem_utils import DatasetLoader
 from config import settings
 
 
+@pytest.mark.acceptance
 def test_load_dataset():
     current = DatasetLoader.handle(str(settings.data_path / "creditcard.csv.zip"))
 
